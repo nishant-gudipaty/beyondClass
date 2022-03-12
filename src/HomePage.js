d77b163a5a7c4f4bb4c4f0e6fd4aef7135678f9c
nishant-gudipaty <64475040+nishant-gudipaty@users.noreply.github.com>
2022-03-12 15:25:02 +0530

    Commit low mem file

--- src/HomePage.js
+++
@@ -1,9 +0,0 @@
-const HomePage = () => {
-    return ( 
-        <div className="home-content bg-dark">
-            <h1>Home page content</h1>
-        </div>
-    );
-}
- 
-export default HomePage;
