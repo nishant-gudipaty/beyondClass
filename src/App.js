7cc7f28600badc6cb8a786297cee1ada4b4e9132
nishant-gudipaty <64475040+nishant-gudipaty@users.noreply.github.com>
2022-03-12 15:39:34 +0530

    Comminting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './index.css';
 // import Navbar from './Navbar';
-import Home from './Home';
-import Test from './test';
-import LogIn from './LogIn';
-import SignUp from './SignUp';
+// import Home from './Home';
+// import Test from './test';
+// import LogIn from './LogIn';
+// import SignUp from './SignUp';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MainPage from './MainPage';
-import HomePage from './HomePage';
+// import MainPage from './MainPage';
+// import HomePage from './HomePage';
 import Posts from './Posts';
 import Forums from './Forums';
 import Create from './createpost';
@@ -17,12 +17,12 @@ const App = () => {
       <div className="App">
         <div className="content">
           <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/test' component={Test}/>
-            <Route exact path='/login' component={LogIn}/>
-            <Route exact path='/signup' component={SignUp}/>
-            <Route exact path='/mainpage' component={MainPage}/>
-            <Route exact path='/homepage' component={HomePage}/>
+            {/* <Route exact path='/' component={Home}/> */}
+            {/* <Route exact path='/test' component={Test}/> */}
+            {/* <Route exact path='/login' component={LogIn}/> */}
+            {/* <Route exact path='/signup' component={SignUp}/> */}
+            {/* <Route exact path='/mainpage' component={MainPage}/> */}
+            {/* <Route exact path='/homepage' component={HomePage}/> */}
             <Route exact path='/posts' component={Posts}/>
             <Route exact path='/myprofile' component={Forums}/>
             <Route exact path='/createpost' component={Create}/>
